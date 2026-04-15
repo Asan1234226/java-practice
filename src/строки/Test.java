3b36ecd5db87c3141a31930c1d035273fea53507
Asan <asan46904@gmail.com>
2026-04-15 22:13:52 +0500

    Добавил чтоб мб конвертировался в ГБ и  добвил единицы измерение в (мин, МБ, шт,тг)

--- src/строки/Test.java
+++ src/строки/Test.java
@@ -14,18 +14,42 @@ public class Test {
 
     static void formatBalance(String response) {
         String[] str = response.split(";");
-        String[] str1 = response.split("=");
 
         for (String s : str) {
-            if (str1.equals("0")) {
+            String[] split = s.split("=");
+            if (split[1].equals("0")) {
                 continue;
+            }
+            if (split[0].equals("balance")) {
+                double balance = Double.parseDouble(split[1]);
+                if (balance < 50) {
+                    System.out.println("Срочно пополните счет на 100 тг и получите +500 минут разговора!");
+                } else if (balance > 50 && balance <= 200) {
+                    System.out.println("Пополните счет на 80 тг и получите +1 ГБ интернета в подарок!");
+                } else if (balance > 200) {
+                    System.out.println("Активируйте роуминг в 15 странах! Пополните счет и получите 50% скидку!");
+                }
+            }
+            if (split[0].equals("megabytes")) {
+                int megabytes = Integer.parseInt(split[1]);
+                if (megabytes >= 1024) {
+                    int gigabyte = megabytes / 1024;
+                    s = "megabytes= " + gigabyte + " ГБ" + " (" + megabytes + ")";
+                }
+            }
+            if (split[0].equals("balance")) {
+                s = split[0] +  "= " + split[1] + " тг";
+            } else if (split[0].equals("minutes")) {
+                s = split[0] + "= " + split[1] + " мин";
+            } else if (split[0].equals("roaming")) {
+                s = split[0] + "= " + split[1] + " МБ";
+            } else if (split[0].equals("sms")) {
+                s = split[0] + "= " + split[1] + " шт";
             }
             System.out.println(s);
         }
-        if (str1.equals("50")) {
-            System.out.println("");
-        }
     }
 }
+
 
 
